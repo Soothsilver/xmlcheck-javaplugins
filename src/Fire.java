6eb60ed2bcb8392c2187daec2a70dab89d8876c4
Petr Hudeček <petrhudecek2010@gmail.com>
2014-11-02 16:06:59 +0100

    november 2, last upload from Hala

--- src/Fire.java
+++ src/Fire.java
@@ -1,8 +1,9 @@
-import name.hon2a.asmp.xquery.Main;
+import name.hon2a.asmp.domsax.Main;
 
 public class Fire {
     public static void main(String[] args)
     {
+        /*
         name.hon2a.asmp.xquery.Main xQuery = new Main();
         System.out.println(
                 xQuery.run(
@@ -13,14 +14,15 @@ public class Fire {
                         }
                 )
         );
-        /*
+        */
+
         name.hon2a.asmp.domsax.Main domSax = new Main();
         System.out.println(
                 domSax.run(
                         new String[] {
                                 "C:\\Apps\\EasyPHP\\data\\localweb\\xmlcheck\\phptests\\plugins\\cases\\DOMSAX\\domSax_correct.zip"
                         } ));
-        */
+
     }
 }
 
